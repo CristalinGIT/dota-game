4da9752148e9d342140ee9b07fa01504a41c6fc5
CristalinGIT <lipasa40@gmail.com>
2026-04-01 20:25:53 +0300

    Add files via upload

---
+++ heroes.js
@@ -0,0 +1,74 @@
+const heroes = [
+{
+name:"abaddon", gender:"male", race:"undead",
+roles:["support","carry"], attr:"strength",
+complexity:1, attack:"melee", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/abaddon_full.png"
+},
+{
+name:"alchemist", gender:"male", race:"goblin",
+roles:["carry"], attr:"strength",
+complexity:1, attack:"melee", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/alchemist_full.png"
+},
+{
+name:"axe", gender:"male", race:"orc",
+roles:["initiator"], attr:"strength",
+complexity:1, attack:"melee", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/axe_full.png"
+},
+{
+name:"bane", gender:"male", race:"demon",
+roles:["support","disabler"], attr:"intelligence",
+complexity:2, attack:"ranged", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/bane_full.png"
+},
+{
+name:"crystal maiden", gender:"female", race:"human",
+roles:["support"], attr:"intelligence",
+complexity:1, attack:"ranged", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/crystal_maiden_full.png"
+},
+{
+name:"drow ranger", gender:"female", race:"undead",
+roles:["carry"], attr:"agility",
+complexity:1, attack:"ranged", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/drow_ranger_full.png"
+},
+{
+name:"earthshaker", gender:"male", race:"beast",
+roles:["initiator"], attr:"strength",
+complexity:2, attack:"melee", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/earthshaker_full.png"
+},
+{
+name:"invoker", gender:"male", race:"human",
+roles:["carry","nuker"], attr:"intelligence",
+complexity:3, attack:"ranged", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/invoker_full.png"
+},
+{
+name:"juggernaut", gender:"male", race:"human",
+roles:["carry"], attr:"agility",
+complexity:1, attack:"melee", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/juggernaut_full.png"
+},
+{
+name:"lion", gender:"male", race:"demon",
+roles:["support","disabler"], attr:"intelligence",
+complexity:2, attack:"ranged", year:2011,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/lion_full.png"
+},
+{
+name:"phantom assassin", gender:"female", race:"human",
+roles:["carry"], attr:"agility",
+complexity:2, attack:"melee", year:2013,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/phantom_assassin_full.png"
+},
+{
+name:"pudge", gender:"male", race:"undead",
+roles:["support","disabler"], attr:"strength",
+complexity:1, attack:"melee", year:2013,
+img:"https://cdn.cloudflare.steamstatic.com/apps/dota2/images/heroes/pudge_full.png"
+}
+];
