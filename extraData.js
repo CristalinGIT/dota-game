d3a50166be54b514f29fa87ec860ae58b4440219
CristalinGIT <lipasa40@gmail.com>
2026-04-01 21:43:40 +0300

    Update extraData.js

--- extraData.js
+++ extraData.js
@@ -1,11 +1,15 @@
 const extra = {
-  "invoker": {role:"mid", complexity:3},
-  "pudge": {role:"support"},
-  "juggernaut": {role:"carry"},
-  "phantom assassin": {role:"carry"},
-  "axe": {role:"offlane"},
-  "crystal maiden": {role:"hard support"},
-  "lion": {role:"support"},
-  "storm spirit": {role:"mid"},
-  "shadow fiend": {role:"mid"}
+  "antimage": {role:"carry", gender:"male", race:"human", complexity:2, year:2011},
+  "axe": {role:"offlane", gender:"male", race:"orc", complexity:1, year:2011},
+  "bane": {role:"support", gender:"male", race:"demon", complexity:2, year:2011},
+  "crystal maiden": {role:"hard support", gender:"female", race:"human", complexity:1, year:2011},
+  "pudge": {role:"support", gender:"male", race:"undead", complexity:1, year:2013},
+  "invoker": {role:"mid", gender:"male", race:"human", complexity:3, year:2011},
+  "juggernaut": {role:"carry", gender:"male", race:"human", complexity:1, year:2011},
+  "phantom assassin": {role:"carry", gender:"female", race:"human", complexity:2, year:2013},
+  "storm spirit": {role:"mid", gender:"male", race:"spirit", complexity:2, year:2011},
+  "lion": {role:"support", gender:"male", race:"demon", complexity:2, year:2011},
+  "zeus": {role:"mid", gender:"male", race:"god", complexity:1, year:2011},
+  "shadow fiend": {role:"mid", gender:"male", race:"demon", complexity:2, year:2011},
+  "sniper": {role:"carry", gender:"male", race:"dwarf", complexity:1, year:2011}
 };
