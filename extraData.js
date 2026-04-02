a84973e0eaf7b756da10f2dcaf773baf4d5e222a
CristalinGIT <lipasa40@gmail.com>
2026-04-02 16:34:46 +0300

    Update extraData.js

--- extraData.js
+++ extraData.js
@@ -1,16 +1,1 @@
 
-const extra = {
-  "antimage": {role:"carry", gender:"male", race:"human", complexity:2, year:2011},
-  "axe": {role:"offlane", gender:"male", race:"orc", complexity:1, year:2011},
-  "bane": {role:"support", gender:"male", race:"demon", complexity:2, year:2011},
-  "crystal maiden": {role:"hard support", gender:"female", race:"human", complexity:1, year:2011},
-  "pudge": {role:"support", gender:"male", race:"undead", complexity:1, year:2013},
-  "invoker": {role:"mid", gender:"male", race:"human", complexity:3, year:2011},
-  "juggernaut": {role:"carry", gender:"male", race:"human", complexity:1, year:2011},
-  "phantom assassin": {role:"carry", gender:"female", race:"human", complexity:2, year:2013},
-  "storm spirit": {role:"mid", gender:"male", race:"spirit", complexity:2, year:2011},
-  "lion": {role:"support", gender:"male", race:"demon", complexity:2, year:2011},
-  "zeus": {role:"mid", gender:"male", race:"god", complexity:1, year:2011},
-  "shadow fiend": {role:"mid", gender:"male", race:"demon", complexity:2, year:2011},
-  "sniper": {role:"carry", gender:"male", race:"dwarf", complexity:1, year:2011}
-};
